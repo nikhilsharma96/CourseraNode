e2d5b318412ae262044e17659f8effe65c14ea06
Nikhil Sharma <atnikhil96@gmail.com>
2020-05-12 12:42:07 +0530

    Express Generator

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan')
 const bodyParser = require('body-parser');
 
+
 const hostname = 'localhost';
 const port = 3000;
 const dishRouter = require('./routes/dishRouter');
@@ -20,8 +21,7 @@ app.use('/leaders',leaderRouter);
 
 app.use(express.static(__dirname+ '/public'));
 
-app.use((req,res,next)=>{
-    
+app.use((req,res,next)=>{    
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>')
